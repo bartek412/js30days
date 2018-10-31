8cacd3b245eb92d29286b158820365550b3a956d
Bartek412 <bartekg.net@gmail.com>
2018-10-31 20:40:40 +0100

    first project done

---
+++ 1st day/main.js
@@ -0,0 +1,20 @@
+const keys = document.querySelectorAll('.key');
+
+function playSound(e) {
+    const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.keys .key[data-key="${e.keyCode}"]`);
+    if (!sound) return;
+
+    key.classList.add('playing');
+    sound.currentTime = 0;
+    sound.play();
+}
+
+function removeTransition(e) {
+    if (e.propertyName != 'transform') return;
+    this.classList.remove('playing');
+}
+
+
+window.addEventListener('keydown', playSound);
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
